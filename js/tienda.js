dda2e5b44b7af9c042e60b018e6db1e5e792ebe9
Corbani Julian <corbani.julian@tecnica7.edu.ar>
2025-07-02 20:26:12 -0300

    me voy a enojar mucho si no funciona. emezamos con el dom y cargamos los productos con el json (falta poner los atributos en las cards con los objetos

--- js/tienda.js
+++ js/tienda.js
@@ -1,4 +1,43 @@
-// Carga los productos a la tienda
+document.addEventListener("DOMContentLoaded",()=>{
+    const endpoint = './data/datos.json';
+    const getProductos = async () =>{
+        try{response = await fetch(endpoint)
+            datos = await response.json()
+            console.log(datos)
 
-// Escuchador            // Espera que carguen tods o scripts vinculados
-document.addEventListener("DOMContentLoaded", () => {})
+           mostrarProductos(datos)
+
+        }catch(error){
+            console.error("Error")
+        }
+}
+getProductos()
+
+    const contenedorProd = document.querySelector('section.productos')
+    const mostrarProductos = (datos) => {
+    datos.forEach (item => {
+        const card = document.createElement('div')
+        card.className = 'card'
+        card.innerHTML = `<div class="top">
+                        <img src="images/productos/cerezas.jpg" alt="">
+                        <p class="titProd">PRODUCTO 2</p>
+                        <p class="descrpcion">Lorem ipsum dolor sit amet consecte. </p>
+                        <a href="opiniones.html" class="resenias">ver reseñas</a>
+                    </div>
+                    <div class="bottom">
+                        <div class="precio">
+                            <span>$1000</span> <button class="favorito"><i
+                                    class="fa-solid fa-heart-circle-check"></i></button></span>
+                        </div>
+                        <div class="agregar">
+                            <div class="amount">
+                                <span class="descrpcion"> Cant </span>
+                                <input type="number" name="cant" id="cant" min="0" value="1">
+                            </div>
+                            <button class="addcarrito"><i class="fa-solid fa-cart-plus"></i></button>
+                        </div>
+                    </div>`
+        contenedorProd.appendChild(card)
+    })
+}
+})
