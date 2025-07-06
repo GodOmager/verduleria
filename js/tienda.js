f882c245bb005109efff108315922e7ce906d123
Corbani Julian <corbani.julian@tecnica7.edu.ar>
2025-07-06 19:51:21 -0300

    agregue los productos eso significa que mañana 7/7/2025 NO VOY A HACER NADA

--- js/tienda.js
+++ js/tienda.js
@@ -15,29 +15,31 @@ getProductos()
 
     const contenedorProd = document.querySelector('section.productos')
     const mostrarProductos = (datos) => {
-    datos.forEach (item => {
-        const card = document.createElement('div')
-        card.className = 'card'
-        card.innerHTML = `<div class="top">
-                        <img src="images/productos/cerezas.jpg" alt="">
-                        <p class="titProd">PRODUCTO 2</p>
-                        <p class="descrpcion">Lorem ipsum dolor sit amet consecte. </p>
-                        <a href="opiniones.html" class="resenias">ver reseñas</a>
-                    </div>
-                    <div class="bottom">
-                        <div class="precio">
-                            <span>$1000</span> <button class="favorito"><i
-                                    class="fa-solid fa-heart-circle-check"></i></button></span>
-                        </div>
-                        <div class="agregar">
-                            <div class="amount">
-                                <span class="descrpcion"> Cant </span>
-                                <input type="number" name="cant" id="cant" min="0" value="1">
-                            </div>
-                            <button class="addcarrito"><i class="fa-solid fa-cart-plus"></i></button>
-                        </div>
-                    </div>`
-        contenedorProd.appendChild(card)
+   datos.forEach(item => {
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.innerHTML = `
+        <div class="top">
+            <img src="${item.image}" alt="${item.title}">
+            <p class="titProd">${item.title}</p>
+            <p class="descrpcion">${item.description}</p>
+            <a href="opiniones.html" class="resenias">ver reseñas</a>
+        </div>
+        <div class="bottom">
+            <div class="precio">
+                <span>$${item.price}</span> 
+                <button class="favorito"><i class="fa-solid fa-heart-circle-check"></i></button>
+            </div>
+            <div class="agregar">
+                <div class="amount">
+                    <span class="descrpcion"> Cant </span>
+                    <input type="number" name="cant" id="cant-${item.id}" min="0" value="1" max="${item.stock}">
+                </div>
+                <button class="addcarrito"><i class="fa-solid fa-cart-plus"></i></button>
+            </div>
+        </div>`
+    contenedorProd.appendChild(card)
+
     })
 }
 })
