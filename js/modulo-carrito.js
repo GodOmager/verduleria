857bf8a96a326a51fe1de8f626cbca4799083c70
Corbani Julian <corbani.julian@tecnica7.edu.ar>
2025-08-13 19:04:50 -0300

    JA les gane, modulo carrito

--- js/modulo-carrito.js
+++ js/modulo-carrito.js
@@ -1,7 +1,18 @@
-const agregarAlCarrito =(item) => {
+const agregarAlCarrito =(item, cant) => {
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    const prodEncontrado = carrito.find (p=>p.id==item.id)
+    if(!prodEncontrado){
+        if(cant>item.stock){
+            console.log("mno hay stock")
+        }else{
+            item.amount = cant
+            carrito.push(item)
+            localStorage.setItem("carrito", JSON.stringify(carrito))
+            console.log("producto agregado al carrito")
+        }
+    }else{
+        console.log("prod agregado al carrito")
+    }
     carrito.push(item);
-    //JSON.stringify convierte los datos a formato JSON
-    localStorage.setItem("carrito", JSON.stringify(carrito));
     alert("Item agregado");
 }
